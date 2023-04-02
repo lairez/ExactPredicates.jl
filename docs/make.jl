e062b4804ffc0f3dcd7e815049f6998eb4fd6830
Pierre Lairez <pierre@lairez.fr>
2023-04-02 14:58:11 +0200

    try to use github actions

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,12 @@ push!(LOAD_PATH, "../src/")
 
 using Documenter, ExactPredicates
 
-makedocs(sitename="ExactPredicates.jl",
-         pages = ["index.md", "api.md"],
-         repo = "https://github.com/lairez/ExactPredicates.jl/blob/master{path}#{line}")
+makedocs(
+    sitename="ExactPredicates.jl",
+    pages = ["index.md", "api.md"],
+    repo = "https://github.com/lairez/ExactPredicates.jl/blob/master{path}#{line}"
+)
 
+deploydocs(
+    repo = "github.com/lairez/ExactPredicates.jl.git",
+)
